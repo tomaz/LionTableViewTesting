6496aefd0a289cb4397cc7a5a7f44df1a79bbd5b
Tomaz Kragelj <tkragelj@gmail.com>
2011-08-30 09:21:21 +0200

    Implemented manually driven example project.

---
+++ Example1/LionTableViewTesting/LionTableViewTestingAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  LionTableViewTestingAppDelegate.h
+//  LionTableViewTesting
+//
+//  Created by Tomaž Kragelj on 8/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+@interface LionTableViewTestingAppDelegate : NSObject <NSApplicationDelegate, NSTableViewDataSource, NSTableViewDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
