b2d4b26782680c9b0ef0df0eb1997e887bcc3269
Tomaz Kragelj <tkragelj@gmail.com>
2011-08-30 09:22:29 +0200

    Implemented details label data handling.

---
+++ Example2/LionTableViewTesting/ItemCellView.m
@@ -0,0 +1,20 @@
+//
+//  ItemCellView.m
+//  LionTableViewTesting
+//
+//  Created by Tomaž Kragelj on 8/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ItemCellView.h"
+
+@implementation ItemCellView
+
+@synthesize detailTextField = _detailTextField;
+
+- (void)dealloc {
+	[_detailTextField release], _detailTextField = nil;
+    [super dealloc];
+}
+
+@end
